9264c8ce27df72524f8fbd0c6f81ed4240cafd0d
ifeonunaekwu ifeoma <ifeonunaekwuifeoma@gmail.com>
2025-11-16 05:54:14 -0500

    Clean repo structure for Vercel + Render

--- server/dist/modules/mail/mail.service.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class MailService {
-    private transporter;
-    constructor();
-    sendMail(dto: {
-        name: string;
-        email: string;
-        message: string;
-    }): Promise<import("nodemailer/lib/smtp-transport").SentMessageInfo>;
-}
